34ac1950bacc6f790bf52f680ab80aa27c3f2115
MACJaveloper <mac.javeloper@gmail.com>
2019-04-25 16:08:54 -0500

    Initial Commit

---
+++ GitMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitMaster
+//
+//  Created by eyc on 25/04/19.
+//  Copyright © 2019 mac.javeloper@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
