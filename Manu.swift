71456a31346fc395a14e9c9af9b8dac5495fda1c
FixingMind5 <aguilarmanu5@gmail.com>
2019-04-25 16:15:54 -0500

    Nuevo archivo por Manu

---
+++ Manu.swift
@@ -0,0 +1,9 @@
+//
+//  Manu.swift
+//  GitMaster
+//
+//  Created by Manuel Alejandro Aguilar Tellez Giron on 4/25/19.
+//  Copyright © 2019 mac.javeloper@gmail.com. All rights reserved.
+//
+
+import Foundation
